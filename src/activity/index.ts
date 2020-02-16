f9ddf770213d3c0a0cf94705db8fd135e4fa1839
Thomas Mean <mail@thomasmean.co.uk>
2020-02-16 23:10:40 +0000

    Log out highlight to explore pivotal responses

--- src/activity/index.ts
+++ src/activity/index.ts
@@ -6,6 +6,9 @@ const router = express.Router();
 
 router.post('/send', async (req, res) => {
     const body: PivotalActivity = req.body;
+
+    // tslint:disable-next-line
+    console.log(body.highlight);
     res.send(body);
 })
 
