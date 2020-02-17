c947350a8e98ac407fe715ff80ed61e421346073
Thomas Mean <mail@thomasmean.co.uk>
2020-02-17 20:20:23 +0000

    Tidyup of json generation

--- src/activity/index.ts
+++ src/activity/index.ts
@@ -12,83 +12,14 @@ router.post('/send', async (req, res) => {
 
         const body: PivotalActivity = req.body;
         const event = body.highlight;
-        const message = `${body.performed_by.name} ${body.highlight} ${body.primary_resources[0].kind} ${body.primary_resources[0].name}`;
 
         if (event === "moved and scheduled") {
-            const json = {
-                "@type": "MessageCard",
-                "@context": "https://schema.org/extensions",
-                "summary": `A Story has been moved to the backlog by ${body.performed_by.name}`,
-                "themeColor": "0078D7",
-                "title": "Story moved to the backlog",
-                "sections": [
-                    {
-                        "activityTitle": `${body.primary_resources[0].name} has been moved to the backlog and is ready for development.`,
-                        "facts": [
-                            {
-                                "name": "Story Name:",
-                                "value": body.primary_resources[0].name
-                            },
-                            {
-                                "name": "Story ID:",
-                                "value": body.primary_resources[0].id
-                            }
-                        ],
-                        "text": "This is now ready for development!"
-                    }
-                ],
-                "potentialAction": [
-                    {
-                        "@type": "OpenUri",
-                        "name": "View Story",
-                        "targets": [
-                            {
-                                "os": "default",
-                                "uri": body.primary_resources[0].url
-                            }
-                        ]
-                    }
-                ]
-            };
+            const json = movedAndScheduledJson(body);
             axios.post(teamsUrl, json);
         }
 
         else if (event === "estimated") {
-            const json = {
-                "@type": "MessageCard",
-                "@context": "https://schema.org/extensions",
-                "summary": `A Story has been Estimated by ${body.performed_by.name}`,
-                "themeColor": "0078D7",
-                "title": "Story Estimated",
-                "sections": [
-                    {
-                        "activityTitle": `${body.primary_resources[0].name} has been estimated. ${body.message}.`,
-                        "facts": [
-                            {
-                                "name": "Story Name:",
-                                "value": body.primary_resources[0].name
-                            },
-                            {
-                                "name": "Story ID:",
-                                "value": body.primary_resources[0].id
-                            }
-                        ],
-                        "text": message
-                    }
-                ],
-                "potentialAction": [
-                    {
-                        "@type": "OpenUri",
-                        "name": "View Story",
-                        "targets": [
-                            {
-                                "os": "default",
-                                "uri": body.primary_resources[0].url
-                            }
-                        ]
-                    }
-                ]
-            };
+            const json = estimatedJson(body);
             axios.post(teamsUrl, json);
         }
         res.send("Success");
@@ -97,5 +28,81 @@ router.post('/send', async (req, res) => {
         res.send("Failed");
     }
 })
+
+const movedAndScheduledJson = (body: PivotalActivity) => {
+    return {
+        "@type": "MessageCard",
+        "@context": "https://schema.org/extensions",
+        "summary": `A Story has been moved to the backlog by ${body.performed_by.name}`,
+        "themeColor": "0078D7",
+        "title": "Story moved to the backlog",
+        "sections": [
+            {
+                "activityTitle": `${body.primary_resources[0].name} has been moved to the backlog and is ready for development.`,
+                "facts": [
+                    {
+                        "name": "Story Name:",
+                        "value": body.primary_resources[0].name
+                    },
+                    {
+                        "name": "Story ID:",
+                        "value": body.primary_resources[0].id
+                    }
+                ],
+                "text": "This is now ready for development!"
+            }
+        ],
+        "potentialAction": [
+            {
+                "@type": "OpenUri",
+                "name": "View Story",
+                "targets": [
+                    {
+                        "os": "default",
+                        "uri": body.primary_resources[0].url
+                    }
+                ]
+            }
+        ]
+    };
+};
+
+const estimatedJson = (body: PivotalActivity) => {
+    return {
+        "@type": "MessageCard",
+        "@context": "https://schema.org/extensions",
+        "summary": `A Story has been Estimated by ${body.performed_by.name}`,
+        "themeColor": "0078D7",
+        "title": "Story Estimated",
+        "sections": [
+            {
+                "activityTitle": `${body.primary_resources[0].name} has been estimated. ${body.message}.`,
+                "facts": [
+                    {
+                        "name": "Story Name:",
+                        "value": body.primary_resources[0].name
+                    },
+                    {
+                        "name": "Story ID:",
+                        "value": body.primary_resources[0].id
+                    }
+                ],
+                "text": `${body.performed_by.name} ${body.highlight} ${body.primary_resources[0].kind} ${body.primary_resources[0].name}`
+            }
+        ],
+        "potentialAction": [
+            {
+                "@type": "OpenUri",
+                "name": "View Story",
+                "targets": [
+                    {
+                        "os": "default",
+                        "uri": body.primary_resources[0].url
+                    }
+                ]
+            }
+        ]
+    };
+};
 
 export default router;
