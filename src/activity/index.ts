cfddff840c04c9424b0ab60229e2e1182a7841eb
Thomas Mean <mail@thomasmean.co.uk>
2020-02-17 19:58:28 +0000

    Messages when moving stories from icebox to backlog

--- src/activity/index.ts
+++ src/activity/index.ts
@@ -14,16 +14,46 @@ router.post('/send', async (req, res) => {
         const event = body.highlight;
         const message = `${body.performed_by.name} ${body.highlight} ${body.primary_resources[0].kind} ${body.primary_resources[0].name}`;
 
-        if (event === "moved") {
-            // tslint:disable-next-line
-            console.log(body.changes);
-            // tslint:disable-next-line
-            console.log(body.changes[0].original_values);
-            // tslint:disable-next-line
-            console.log(body.changes[0].new_values);
+        if (event === "moved" && body.changes[0].original_values.current_state === "unscheduled" && body.changes[0].new_values.current_state === "unstarted" && body.kind === "story") {
+            const json = {
+                "@type": "MessageCard",
+                "@context": "https://schema.org/extensions",
+                "summary": `A Story has been moved to the backlog by ${body.performed_by.name}`,
+                "themeColor": "0078D7",
+                "title": "Story Moved to the backlog",
+                "sections": [
+                    {
+                        "activityTitle": `${body.primary_resources[0].name} has been moved to the backlog and is ready for development. ${body.message}.`,
+                        "facts": [
+                            {
+                                "name": "Story Name:",
+                                "value": body.primary_resources[0].name
+                            },
+                            {
+                                "name": "Story ID:",
+                                "value": body.primary_resources[0].id
+                            }
+                        ],
+                        "text": message
+                    }
+                ],
+                "potentialAction": [
+                    {
+                        "@type": "OpenUri",
+                        "name": "View Story",
+                        "targets": [
+                            {
+                                "os": "default",
+                                "uri": body.primary_resources[0].url
+                            }
+                        ]
+                    }
+                ]
+            };
+            axios.post(teamsUrl, json);
         }
 
-        if (event === "estimated") {
+        else if (event === "estimated") {
             const json = {
                 "@type": "MessageCard",
                 "@context": "https://schema.org/extensions",
